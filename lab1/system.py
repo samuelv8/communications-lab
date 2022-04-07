1af8d892b250a570576b4df2e8609dc19cb5b51c
samuelv8 <78799492+samuelv8@users.noreply.github.com>
2022-04-06 23:00:37 -0300

    Allow system to work with different algorithms

--- lab1/system.py
+++ lab1/system.py
@@ -1,14 +1,21 @@
-from encoder import Hamming as HammingEncoder
+from encoder import Hamming as HammingEncoder, Custom as CustomEncoder
 from channel import Channel
-from decoder import Hamming as HammingDecoder
+from decoder import Hamming as HammingDecoder, Custom as CustomDecoder
 import numpy as np
 
 
 class System:
-    def __init__(self, p: float = 0.0) -> None:
-        self.encoder = HammingEncoder()
+    def __init__(self, p: float = 0.0, type: str = 'hamming') -> None:
         self.channel = Channel(p)
-        self.decoder = HammingDecoder()
+        if (type == 'hamming'):
+            self.encoder = HammingEncoder()
+            self.decoder = HammingDecoder()
+        elif (type == 'custom'):
+            self.encoder = CustomEncoder()
+            self.decoder = CustomDecoder()
+        else:
+            raise NotImplementedError("There's no system with the requested type")
+
 
     def _encode_message(self, msg: np.ndarray) -> np.ndarray:
         return self.encoder.encode(msg)
